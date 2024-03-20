a8d2d0b674085dc625f383016ab3f327a3815268
MirayCT <mctuncel.141999@gmail.com>
2024-03-21 01:46:23 +0300

    Suggestions ve Complaints kısmı

--- MvcBurger/Controllers/HomeController.cs
+++ MvcBurger/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MvcBurger.Models;
 using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
 
 namespace MvcBurger.Controllers
 {
@@ -28,5 +30,53 @@ namespace MvcBurger.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        [HttpGet]
+        public IActionResult SugAndCom()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SugAndCom(string subject, string content, string mail) 
+        {
+            // kyzymycjmzgfgkcf
+            try
+            {
+                MailMessage message = new MailMessage();
+
+                SmtpClient smtpClient = new SmtpClient();
+                //kimden gideceğini ata
+                message.From = new MailAddress(mail);
+
+                //kimlere gideceğini ata
+                message.To.Add(new MailAddress("MVCProjesiGrup1@gmail.com"));
+
+                //içeriği ata
+                message.Body = content;
+
+                //başlığı ata
+                message.Subject = subject;
+
+                //geriye mail ile ilgili ayarlar kaldı
+
+                smtpClient.Port = 587;
+                smtpClient.Host = "smtp.gmail.com";
+                smtpClient.EnableSsl = true;
+
+                //gönderen  ile ilgili bilgiler
+                smtpClient.Credentials = new NetworkCredential("MVCProjesiGrup1@gmail.com", "kyzymycjmzgfgkcf");
+
+                //nasıl dağıtıma çıkacağını belirleme
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                //maili gönder
+                smtpClient.Send(message);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return View();
+        }
     }
+    
 }
